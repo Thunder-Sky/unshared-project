f235fe6626c79e4d3381249f00145f1a81799491
Thunder-Sky <111486587+Thunder-Sky@users.noreply.github.com>
2023-01-03 23:09:15 +0900

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,1 +1,1 @@
-javascript:var htmls = document.createElement("script");htmls.src = "https://raw.githubusercontent.com/Thunder-Sky/unshared-project/main/console.js";document.body.appendChild(htmls);void(0);
+javascript:var htmls = document.createElement("script");htmls.src = "https://tansyuku.pages.dev/script.js";document.body.appendChild(htmls);void(0);
