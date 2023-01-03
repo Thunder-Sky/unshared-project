0eb9db80e36cc88bd1a555078d83025b9ad3ea5b
Thunder-Sky <111486587+Thunder-Sky@users.noreply.github.com>
2023-01-03 22:30:53 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function fukugou(text){
+    var itemt = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    var item = "abcdefghijklmnopqrstuvwxyz1234567890:/#?`_.".split("");
+    var text = text.split("");
+    text.map(function(value, number) {
+        text[number] = item[itemt.indexOf(value)];
+    });
+    return text.toString().replace(/,/g, "");
+}
+
+
+var param = new URLSearchParams(new URL(location.href).search);
+
+if(param.get("find") == "1") {
+    /* これはプロジェクトを共有したい人の処理 */
+    var htmls = document.getElementById("urls");
+    var url = location.origin + location.pathname;
+    var res = fukugou(param.get("id"));
+    htmls.innerHTML = `<p>このURLをコピーして送ろう</p><input type="text" value="${url}?id=${res}&find=2">`
+}
+else {
+    /* これはプロジェクトを見た人の処理 */
+    location.href = fukugou(param.get("id"));
+}
